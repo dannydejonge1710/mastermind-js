57d06f613fd0e951bb4e173c1530ed2df2ca2e94
DESKTOP-AT2NR95\Danny <dannydejonge1710@gmail.com>
2017-12-14 11:49:39 +0100

    no message

--- js/game.js
+++ js/game.js
@@ -2,14 +2,38 @@ var kleurenLijst = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
 var count = 6;
 
 
+var xvar = Math.floor((Math.random() * 6) + 0);
+document.getElementById('test').style.backgroundColor = kleurenLijst[xvar];
+
+var xvar1 = Math.floor((Math.random() * 6) + 0);
+document.getElementById('test1').style.backgroundColor = kleurenLijst[xvar1];
+
+var xvar2 = Math.floor((Math.random() * 6) + 0);
+document.getElementById('test2').style.backgroundColor = kleurenLijst[xvar2];	
+
+var xvar3 = Math.floor((Math.random() * 6) + 0);
+document.getElementById('test3').style.backgroundColor = kleurenLijst[xvar3];
 
 function addColor(frituurpan){
 
 	var kleur = kleurenLijst [frituurpan];
 	document.getElementById('option' + count).style.backgroundColor = kleur;
+
+	if (count) {
+		if (frituupan = xvar) {
+			alert('good');
+		} else if (frituurpan = xvar1) {
+			alert('almost good');
+		} else if (frituurpan = xvar2) {
+			alert('almost good');
+		} else if (frituurpan = xvar3) {
+			alert('almost good');
+		} else {
+			alert('false');
+		}
+	}
+
 	count ++;
-}
-
-function addComb(){
 
 }
+
