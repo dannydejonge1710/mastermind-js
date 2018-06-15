f23baa44dc36a6d38d9e3824d99504ecc58255c6
DESKTOP-0ONJAOC\Danny <dannydejonge1710@gmail.com>
2018-06-15 21:24:55 +0200

    Aanpassingen aan checkfunctie

--- js/game.js
+++ js/game.js
@@ -8,18 +8,13 @@ var gecontroleerd;
 var checked= [];
 var winArray = [];
 
-//Boord genereren
+
 
 for(var a=0; a<12;a++) {
 
-	// div voor row bolletje wordt aangemaakt.
 	var row = document.createElement("div");
 	
-	// krijgt een id myRow
-
 	row.setAttribute("id", "myRow" + a);
-
-	// de divs worden in de div mainboard gezet.
 
 	document.getElementById('mainBoard').appendChild(row);
 
@@ -55,94 +50,7 @@ for(var c=0; c<12;c++) {
 	}
 }
 
-console.log('1 Alle bolletjes zijn geladen');
 
-// Krijgt een parameter binnen met een nummer en wordt frituurpan genoemd
-
-function addColor(frituurpan){ 
-
-	console.log('2 Er is een kleur doorgegeven en via de onclick functie is de functie addColor opgeroepen.');
-
-// Haalt uit de array met kleuren het nummer wat is doorgegeven in de parameter en stopt het in de Var kleur 
-
-	var kleur = kleurenLijst [frituurpan];	
-
-	console.log('3 Toen je op de kleur geklikt hebt, is er een parameter meegegeven met een nummer, en dit nummer haalt die uit de array kleurenlijst met alle kleuren, vervolgens wordt deze kleur in de variabele kleur gestopt.')
-
-//	Krijgt het element bij id en dan vervolgens bij class en veranderd dit in de kleur
-
-	document.getElementById('myRow' + countRow).getElementsByClassName('myBolletje' + count)[0].style.backgroundColor = kleur;
-
-
-	console.log('4 Het bolletje veranderd naar de gekozen kleur.');
-// Als dit gebeurt is gaat de count 1 omhoog en gaat die naar de volgende myBolletje
-
-	count++;
-
-	console.log('5 De count is opgehoogd zodat hij automatisch naar het volgende lege bolletje gaat');
-
-// De kleur wordt in de lege guess array gepushed
-
-	guess.push(kleur);
-
-	console.log(guess);
-
-	console.log('6 de gekozen kleur is in de lege guess array gepushed');
-
-	
-
-// Als de count 4 heeft bereikt voert hij de if statement uit
-
-	if (count == 4) {
-
-	console.log('7 nu het 4e bolletje bereikt is voert hij de ifstatement uit. DE IFSTATEMENT VOERT DEZE DINGEN UIT');
-
-
-// De count wordt weer op nul gezet zodat die weer bij het begin begint in de volgende row
-
-		count = 0;
-
-	console.log('8 de count is weer naar 0 geplaatst zodat hij weer bij het eerste bolletje begint.');
-// De row wordt opgehoogd en dan gaat hij naar de volgende row 
-
-		countRow++;
-
-	console.log('9 de countrow is 1 opgehoogd zodat hij naar de volgende row gaat.');
-	
-// de globale Var gecontroleerd krijgt een value met de checkFunction erin
-
-	console.log('10 De checkfunctie wordt aangeroepen en wat deze returned wordt opgeslagen in de variabele gecontroleerd');
-
-	gecontroleerd = checkFunction();
-
-// Hij voert dan de checkfuntion uit en geeft de checked array mee aan de geefFeedback functie
-	console.log('25 de code is weer terug in de addColor functie');
-	console.log('26 Nu de checkfuntie is uitgevoerd en de checked array in de gecontroleerd variabele staat, geeft de code de gecontroleerd variabele mee aan de geeffeedback functie.');
-
-	geefFeedback(gecontroleerd);
-
-// Hierna hoogt die de counter die de row van de check bolletjes ophoofd
-	console.log('31 de code is weer terug in de addColor functie');
-
-	countEr++;
-
-	console.log('32 de counter wordt opgehoogd zodat de checkbolletjes naar de volgende row gaan.');
-
-// de guess array wordt vervolgens weer leeggemaakt.
-
-	 
-
-	guess=[];
-
-	console.log('guess waar de aangeklikt kleuren in waren gepushed word weer geleegd');
-
-
-
-	}
-
-}
-
-//Random kleurencode genereren
 
 var w = Math.floor((Math.random() * 6));
 document.getElementById('test').style.backgroundColor = kleurenLijst[w];
@@ -160,111 +68,101 @@ var randomArray = [w, x, y, z];
 
 
 
+function addColor(frituurpan){ 
+
+	var kleur = kleurenLijst [frituurpan];	
+	document.getElementById('myRow' + countRow).getElementsByClassName('myBolletje' + count)[0].style.backgroundColor = kleur;
+	count++;
+
+	guess.push(kleur);
+	console.log(guess);
+
+	if (count == 4) {
+		count = 0;
+		countRow++;
+
+		gecontroleerd = checkFunction();
+		geefFeedback(gecontroleerd);
+
+		countEr++;
+
+		guess=[];
+	}	
+}
 
 
-//Check functie
 
 function checkFunction(){
 
-// de globale Var answercopy krijgt een value van copyAnswer function en hij gaat naar de functie 
-// dus als de checkfuntie wordt uitgevoerd gaat hij direct naar copyAnswer
-	console.log('11 de code is nu in de checkfunctie.')
-	console.log('12 als hij in de checkfuntie is gekomen roept hij als eerst de copyAnswer functie op en wat deze functie returned wordt in functie answercopy gestopt');
-
-	answercopy = copyAnswer();
-
-	console.log('16 de array is opgeslagen in de answercopy array');
-
-// de array checked is hier leeg en staat ook globaal
-
-	console.log('17 op dit moment is de checked array leeg.');
+	randomarraycopy = copyRandomArray();
 
 	checked = [];
 
 	for (var i = 0; i < 4; i++){
 
-// De answer copy heeft het antwoord en de tel hoogt het op om voor elke plek het antwoord te hebben.
-// Als dit gelijk is aan de guess (op die plek). en in de guess zit kleur gepushed 
+		if (guess[i] == kleurenLijst[randomarraycopy[i]]) {
 
-	console.log('18 de if statement gaat nu voor elk bolletje appart kijken of de aangeklikte kleur gelijk is aan de kleur van het random antwoord.')
+			//als het precies op de goede plek staat pushed hij een 2 in de checked array
 
-		if (guess[i] == kleurenLijst[answercopy[i]]) {
-
-			// als hij de twee in checked pushed betekend dit dat het goed is
-			console.log('19 de aangeklikte kleur staat op de goede plek, er word nu een 2 gepushed in de checked array');
 			checked.push(2);
-			answercopy[i] = -1;
-			console.log('het antwoord dat overeenkomt is uit de array gehaald zodat het deze niet nog een keer controleerd')
+
+			//de kleur die op de goede plek stond wordt dan ook uit de randomarraycopy gehaald door er -1 van te maken kan het dus nooit meer gelijk zijn aan iets
+			//de kleur die op de goede plek stond wordt dan ook uit de guess array gehaald door er "bullshit" van te maken kan het dus nooit meer gelijk zijn aan iets
+
+
+			guess[i] = "bullshit";
+			randomarraycopy[i] = -1;
+
+			//hoe ziet die random array er dan uit stel hij zou op de tweede plek een groene vinde
+			//guess =  5  bulllshit  2  bullshit
+			//randomarraycopy = 5 -1 2 -1
+			// checked = 2 , 2
 		}
 	}
 
-	console.log('20 er komt nu een if statement die kijkt of er 2 tjes in de array staan. Als hij op een plek geen tweetje vind voert hij de volgende if statement uit.')
-
 	for (var j = 0; j < 4; j++){
-
-		// hij checked voor elke van de 4 plekke  in de array of er een 2 staat, zo niet gaat hij door
 
 		if(checked[j] != 2){
 
-			console.log('21 er is hier geen 2 aangetoond. Nu gaat die in de volgende if statement checken of hij er helemaal niet inzit of dat hij er wel inzit maar op een andere plek.')
-			console.log('22 bij de volgende ifstatement kijkt hij weer of er van de overgebleven kleuren 1 inzit de overeen komt. aangezien de gene die op de goede plek staan al gecontroleerd zijn kan de if statement de overgebelven bolletjes controleren en vervolgens bij de egne die overeenkomen een 1 meegeven ')
 			for (var i = 0; i<4; i++){
 
-				//vervolgens gaat die bij het nummer j waar die geen 2 vond alles
-
-				if (guess[i] == kleurenLijst[answercopy[j]]){
-
-				console.log('23 de gene die in deze if statement overeenkomen worden naar de checked array gepushed.')
+				if (guess[i] == kleurenLijst[randomarraycopy[j]]){
 
 				checked.push(1);
-				answercopy[j] = -1;
 
-				console.log('De kleur is nu uit de answer array gehaald, want stel dat de speler 2 keer dezelfde kleur aanklikt mag hij niet 2x zoveel zwarte bolletjes creeeren')
-				
-				break;
-				//als het nummer van groen bv 3 is en groen komt er 2 keer in dan krijg je dus 2 keer een zwart bolletje. en daarom moet het antwoord er ook gelijk uit gehaald worden, zodat je niet als de speler 2 keer groen aanklikt , 4 zwarte bolletjes krijgt. 
+				guess[i] = "bullshit";
+				randomarraycopy[j] = -1;	
 				}
 			}
 		}
 	}
-console.log('24 de checked array wordt gereturned en word in de variabele gecontroleerd gestopt.');
 return checked;
 }
 
-function copyAnswer()
+
+
+function copyRandomArray()
 {
+	var randomarraycopy = [];
 
-	// var answercopy wordt een lege array
-
-	console.log('13 de code is nu in de copyanswer functie.')
-
-	var answercopy = [];
-
-	//ranDom array heeft een lengte van 4 dus hij hoofd de tel3 4 keer op van 0 naar 3 en zo worden alle waardes in de answercopy gezet
-	console.log('14 de lengte van de random array is 4 hij zet nu de dingen in de random array in de lege answercopy array.');
 	for(z=0;z<randomArray.length; z++)
 	{
-		answercopy[z] = randomArray[z];
+		randomarraycopy[z] = randomArray[z];
 	}
-	//vervolgens geeft hij answercopy terug. Hier staat nu het random antwoord in.
-
-	console.log('15 Hij gaat weer terug naar de checkfuntie en geeft answercopy mee ');
-	return answercopy;
+	return randomarraycopy;
 }
 
 
 
 function geefFeedback(gecontroleerdeArray){
-	console.log('27 De code is nu in de geefFeedback functie');
-	console.log("28 gecontroleerde array=" + gecontroleerdeArray);
+
 	for(f=0; f < guess.length; f++){
+
 		if (gecontroleerdeArray[f] == 2) {
 			document.getElementById('myCheckRow'+countEr).getElementsByClassName('myCheckBolletje' + f )[0].style.backgroundColor = "red";
-			console.log('29.1 de if statement ziet een 2 in de array en maakt het bolletje rood');
 		}
 		if (gecontroleerdeArray[f] == 1) {
 			document.getElementById('myCheckRow'+countEr).getElementsByClassName('myCheckBolletje' + f)[0].style.backgroundColor = "black";
-			console.log('29.2 de if statement ziet een 1 in de array en maakt het bolletje zwart');
 		}
 	}
 
@@ -274,7 +172,6 @@ function geefFeedback(gecontroleerdeArray){
 
 	console.log(gecontroleerd);
 
-	console.log('30 de gecontroleerd array met de 1tjes en 2tjes zijn weer geleegd.');
 
 }
 
@@ -293,9 +190,7 @@ function winFunctie(gecontroleerdwin){
 			alert('YOU WIN!!!');
 			reloading();
 		}
-
 	}
-
 }
 
 function reloading(){
